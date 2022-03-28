2a264cbba1ebe02c88c0fcddacb405b25d0ad960
Scheuer Patrik <70636500+spatrik2001@users.noreply.github.com>
2022-03-28 10:05:21 +0200

    Create auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,175 @@
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto');
+
+const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: 'API kulcs helye'
+  }
+}));
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'SpaceY · Bejelentkezés',
+    errorMessage: req.flash('error')
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'SpaceY · Regisztráció',
+    errorMessage: req.flash('error')
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'Helytelen email vagy jelszó.');
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+    })
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash('error', 'Ezzel az email címmel már regisztráltak.');
+        return res.redirect('/signup');
+      }
+      else if (password != confirmPassword)
+      {
+        req.flash('error', 'A jelszavak nem egyeznek meg.');
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+            role: 'User'
+        })
+        return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+      return transporter.sendMail({
+        to: email,
+        from: 'scheuer.patrik@students.jedlik.eu',
+        subject: 'Sikeres regisztráció',
+        html: '<h1>Köszönjük, hogy regisztrált weboldalunkra!</h1>'
+      })
+    })
+  })
+  .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'SpaceY · Elfelejtett jelszó',
+    errorMessage: req.flash('error')
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'Ezzel az email címmel nincs felhasználó regisztrálva.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'scheuer.patrik@students.jedlik.eu',
+          subject: 'Jelszó helyreállítás',
+          html: `
+              <p>You requested a password reset</p>
+              <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>`
+        })
+      })
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() }})
+    .then(user => {
+      if (!user)
+      {
+        return res.redirect('/');
+        // return next();
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'SpaceY · Új jelszó igénylése',
+        errorMessage: req.flash('error'),
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetuser;
+
+  User.findOne({ resetToken: passwordToken, resetTokenExpiration: { $gt: Date.now() }, _id: userId })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+};
