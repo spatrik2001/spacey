e92efc944d1044d43ac7e7ea2114a6c70909fb70
spatrik2001 <scheuer.patrik@gmail.com>
2022-03-30 23:21:34 +0200

    Adding some features like map to contact

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// favicon módosítása
 const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || false;
 const favicon = document.getElementById('favicon');
 
@@ -10,3 +11,16 @@ function switchIcon(darkMode) {
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => switchIcon(e.matches));
 switchIcon(darkMode);
+
+// Google Maps
+function basement() {
+    const jedlik = { lat: 47.68230, lng: 17.63021 };
+    const map = new google.maps.Map(document.getElementById('spaceyBasement'), {
+        center: jedlik,
+        zoom: 18
+    });
+    const marker = new google.maps.Marker({
+        position: jedlik,
+        map: map
+    });
+}
