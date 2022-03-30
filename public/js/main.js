86516fe8925c90f29dba77bf3854b7780d60973f
spatrik2001 <scheuer.patrik@gmail.com>
2022-03-30 19:39:55 +0200

    Editing css', adding some methods, changing some images

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
+const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || false;
+const favicon = document.getElementById('favicon');
 
+function switchIcon(darkMode) {
+    if (darkMode)
+        favicon.href = '/img/favicon-light.png';
+    else
+        favicon.href = '/img/favicon-dark.png';
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => switchIcon(e.matches));
+switchIcon(darkMode);
